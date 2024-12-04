448845d6e7f6d674848610aeca935af1ba4da841
elimulo <mulocher.elise@gmail.com>
2024-12-04 12:11:37 +0100

    test unitaires

--- test_unitaires/test_model.py
+++ test_unitaires/test_model.py
@@ -6,7 +6,7 @@ import numpy as np
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 # Example model URI
-model_uri = 'runs:/f8d1df805a2a4209b7ec662c6ab1dc00/lightgbm_model_final'
+model_uri = 'runs:/f18925492b974af4aaae5fe4d38cd151/lightgbm_model_final'
 
 # This function is used to load the model from the remote server
 @pytest.fixture(scope='module')
@@ -21,7 +21,7 @@ def test_model_loads(loaded_model):
 # Test 2: Check if model prediction works 
 def test_model_prediction(loaded_model):
     
-    input_data = [263698,2,0.0,2,0.0,127039.5,114750.0,0.0,-1397.4615384615386,0.0,False,664636.4619230769,0.0,1.0,11.0,True,0.0,False,False,46080.0,0.0,118165.5,0.0,0,
+    input_data = np.array([2,0.0,2,0.0,127039.5,114750.0,0.0,-1397.4615384615386,0.0,False,664636.4619230769,0.0,1.0,11.0,True,0.0,False,False,46080.0,0.0,118165.5,0.0,0,
                   0.0,221400.0,22.0,0.0,0,3483.0,173880.0,False,9814.5,False,False,False,0.0,False,False,0.0,0.0,False,False,15.0,0.0,10.125,0.0,False,0.125,0.0,-788.25,
                   0.0,0.0,False,True,0.0,False,False,-10916,10.0,False,0.0,0.0,0.0,True,0.0,0.0,False,0.0,247500.0,1,48280.5,12092.104285714286,0.0,0.0,False,119084.625,
                   0.0,0.0,False,False,False,-438.0,0.0,False,False,6038.595,False,-3534,True,False,0.0,-65.0,0.0,False,False,0.0,0.0,324801.225,False,0.0,0.0,False,0.5,
@@ -42,8 +42,8 @@ def test_model_prediction(loaded_model):
                   False,0.6925590674998008,True,0.0,0.0,0.75,0,0.0,0.6923076923076923,0.0,0,0.0,0.0,0.0,1,0,0.3,0,0.0,0.0,0.0,False,False,0.0,1.0423496353783368,0.0,0.0,1,
                   False,0.2307692307692307,0.2,12404.115,0.0,0.0,30.0,False,0.0,0.0,False,0.0,0.7142857142857143,0,False,True,0.0,False,False,7875000.0,False,5750.775,13,
                   False,False,1.0,-3.0,False,37035.4725,0.0,12726.201315789473,-28.571428571428573,0.0,0.0769230769230769,0.2,0.0,0,31118.0,0.0,0.0,False,0.0,True,0.0,0,
-                  0.0,False,0.1048607239885971,0.0,212493.6,False,0.0,False,0.9044993450513296,False,0.0,31.0]  
-    prediction = loaded_model.predict(input_data)
+                  0.0,False,0.1048607239885971,0.0,212493.6,False,0.0,False,0.9044993450513296,False,0.0,31.0] )
+    prediction = loaded_model.predict(input_data.reshape(1, -1))
 
     # Assert prediction shape or values depending on model output
     assert prediction is not None, "Prediction should not be None"
