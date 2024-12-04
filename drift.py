afbf3e5f66c6914b72fd2d38091b8b251b70bf96
elimulo <mulocher.elise@gmail.com>
2024-12-04 13:03:20 +0100

    test sans evidently

--- drift.py
+++ drift.py
@@ -3,22 +3,16 @@ from evidently.metrics import DataDriftTable
 
 import pandas as pd
 
-# # Load datasets
-# train_data = pd.read_csv("application_train.csv")
-# test_data = pd.read_csv("application_test.csv")
+# Load datasets
+train_data = pd.read_csv("application_train.csv")
+test_data = pd.read_csv("application_test.csv")
 
-# # Define Evidently report
-# report = Report(metrics=[DataDriftTable()])
+# Define Evidently report
+report = Report(metrics=[DataDriftTable()])
 
-# # Generate report
-# report.run(reference_data=train_data, current_data=test_data)
+# Generate report
+report.run(reference_data=train_data, current_data=test_data)
 
-# # Save as HTML
-# report.save_html("data_drift_report.html")
+# Save as HTML
+report.save_html("data_drift_report.html")
 
-from evidently.dashboard import Dashboard
-from evidently.dashboard.tabs import DataDriftTab
-
-dashboard = Dashboard(tabs=[DataDriftTab()])
-dashboard.calculate(train_data, current_data)
-dashboard.show()
