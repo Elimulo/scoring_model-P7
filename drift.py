50b7f95030972246c28420359df325a20b09f5ab
elimulo <mulocher.elise@gmail.com>
2024-12-04 14:31:16 +0100

    tests deploy

--- drift.py
+++ drift.py
@@ -1,18 +1,24 @@
-from evidently.report import Report
-from evidently.metrics import DataDriftTable
+from evidently.report import  Report 
+from evidently.metric_preset import DataDriftPreset
+from evidently import ColumnMapping
 
-import pandas as pd
 
-# Load datasets
-train_data = pd.read_csv("application_train.csv")
-test_data = pd.read_csv("application_test.csv")
+X_train = pd.read_csv('../data/application_train.csv', index_col='SK_ID_CURR').drop(columns=['TARGET'])
+X_test = pd.read_csv('../data/application_test.csv', index_col='SK_ID_CURR')
 
-# Define Evidently report
-report = Report(metrics=[DataDriftTable()])
+mask = (info_df['Dtype'].str.contains('str')) | (info_df['n_unique']==2)
+categorical = list(info_df.loc[mask,:].index)
+numerical = [idx for idx in info_df.index if idx not in categorical]
+column_mapping = {'categorical_features': categorical,
+                  'numerical_features': numerical }
 
-# Generate report
-report.run(reference_data=train_data, current_data=test_data)
+ColumnMapping(**column_mapping)
 
-# Save as HTML
-report.save_html("data_drift_report.html")
+
+data_drift_report = Report(metrics=[DataDriftPreset()])
+data_drift_report.run(reference_data=X_train, 
+                      current_data=X_test,
+                       column_mapping=ColumnMapping(**column_mapping) )
+data_drift_report.save('../data_drift_report.html')
+data_drift_report.show()
 
